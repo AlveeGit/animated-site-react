2905cfbc413a7db5d4c75c91c642dfeb0f5c934f
Md. Alvee Kabir <alveekabir1@gmail.com>
2025-10-23 16:28:45 +0600

    added hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,82 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React from "react";
+
+const Hero = () => {
+  useGSAP(() => {
+    // GSAP animations can be added here
+    const hreoSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    hreoSplit.chars.forEach((char) => {
+      char.classList.add("text-gradient");
+    });
+    gsap.from(hreoSplit.chars, {
+      yPercent: 100,
+      duration: 2,
+      ease: "expo.out",
+      stagger: 0.1,
+    });
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.5,
+      ease: "expo.out",
+      stagger: 0.1,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+  }, []);
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">Mojo</h1>
+
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cold . Delicious . Simple</p>
+              <p className="subtitle">
+                The perfect cocktail <br /> for any occasion
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Inventore itaque, repellendus quibusdam nobis repellat quis.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
