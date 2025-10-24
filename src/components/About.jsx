d7219895ed0443399c034d09913165a72c6e28e3
Md. Alvee Kabir <alveekabir1@gmail.com>
2025-10-24 16:20:26 +0600

    added about section with animation

---
+++ src/components/About.jsx
@@ -0,0 +1,97 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
+const About = () => {
+  useGSAP(() => {
+    // GSAP animations can be added here
+    const tittleSplit = SplitText.create("#about h2", {
+      type: "words",
+    });
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+
+    scrollTimeline
+      .from(tittleSplit.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        ease: "expo.out",
+        stagger: 0.1,
+      })
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          stagger: 0.02,
+        },
+        "-=0.5"
+      );
+  }, []);
+
+  return (
+    <div id="about">
+      <div className="mb-16 md:px-0 px-5">
+        <div className="content">
+          <div className="md:col-span-8">
+            <p className="badge">Best Drinks</p>
+            <h2>Take a sip and immerse yourself in a world of flavor</h2>
+          </div>
+
+          <div className="sub-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              euismod, nisl euismod aliquet luctus, nisl nisl euismod nisl,
+              euismod nisl euismod nisl.
+            </p>
+
+            <div>
+              <p className="md:text-3xl text-xl font-bold">
+                <span>4.5</span>/5
+              </p>
+
+              <p className="text-sm text-white-100">Based on 2,000+ Reviews</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="top-grid">
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt1.png" alt="grid-image-1" />
+        </div>
+
+        <div className="md:col-span-6">
+          <div className="noisy" />
+          <img src="/images/abt2.png" alt="grid-image-2" />
+        </div>
+
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt5.png" alt="grid-image-5" />
+        </div>
+      </div>
+      <div className="bottom-grid">
+        <div className="md:col-span-8">
+          <div className="noisy" />
+          <img src="/images/abt3.png" alt="grid-image-3" />
+        </div>
+
+        <div className="md:col-span-4">
+          <div className="noisy" />
+          <img src="/images/abt4.png" alt="grid-image-4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
